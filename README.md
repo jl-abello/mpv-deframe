9785d072faddbaa4733e5e7f5c8b90fd5aead023
b4zz4 <bazza@riseup.net>
2016-06-23 15:13:12 -0300

    inicio

---
+++ README.md
@@ -0,0 +1,17 @@
+# mpv sin marcos de youtube
+
+~~~
+mkdir -p ~/.config/mpv/scripts/
+cp * ~/.config/mpv/scripts/
+~~~
+
+Requiere:
+---------
+
+* ffmpegthumbnailer
+* imagemagick
+
+--
+
+**Bitcoin:** 19qkh5dNVxL58o5hh6hLsK64PwEtEXVHXs
+
---
+++ deframe.lua
@@ -0,0 +1,15 @@
+-- default keybinding: n
+-- add the following to your input.conf to change the default keybinding:
+-- keyname script_binding auto_load_deframe
+local utils = require 'mp.utils'
+function load_deframe_fn()
+    mp.osd_message("Search frame...")
+    subl = "/home/fuquo/.config/mpv/scripts/deframe"
+    t = {}
+    t.args = {subl,mp.get_property("path")}
+    res = utils.subprocess(t)
+    mp.command("vf add @%s:crop=" .. res.stdout)
+    mp.osd_message(res.stdout)
+end
+
+mp.add_key_binding("g", "auto_load_deframe", load_deframe_fn)
